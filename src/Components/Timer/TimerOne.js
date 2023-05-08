5291f1b40179277fe748c3e98f68dc773ca54d6e
Aayushi-Mittal <aayushimittal088@gmail.com>
2023-05-09 03:14:16 +0530

    methods-in-updating-phase

--- src/Components/Timer/TimerOne.js
+++ src/Components/Timer/TimerOne.js
@@ -3,46 +3,36 @@ import React from "react";
 export default class TimerOne extends React.Component {
   constructor() {
     super();
-
-    this.state = {
-      time: 0
-    };
-
-    this.timer = null;
+    console.log("Timer Constructor");
   }
 
+  static getDerivedStateFromProps(props, state) {
+    console.log("Timer getDerivedStateFromProps");
+    return null;
+  }
 
   componentDidMount() {
     console.log("Timer ComponentDidMount");
-    console.log("_________________________________");
-    
-    this.timer = setInterval(() => {
-      this.setState((prevState) => ({ time: prevState.time + 1 }));
-    }, 1000);
   }
 
   getSnapshotBeforeUpdate(prevProp, prevState) {
     console.log("Timer getSnapshotBeforeUpdate");
-    console.log("_________________________________");
     return null;
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log("Timer shouldComponentUpdate");
-    console.log("_________________________________");
     return true;
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     console.log("Timer componentDidUpdate");
-    console.log("_________________________________");
   }
 
   render() {
     return (
       <div>
-        <h2>Time Spent: {this.state.time}</h2>
-        {new Date(this.state.time * 1000).toISOString().slice(11, 19)}
+        <h2>Timer</h2>
       </div>
     );
   }
